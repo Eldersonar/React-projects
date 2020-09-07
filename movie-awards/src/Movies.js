1e8fb1e49069dfebcd9e5603cbc42d7d41624f04
Slava Nazarenko <slavaecu@gmail.com>
2020-09-07 12:49:05 -0600

    fixed movies component and the app component. Movies search works as expected

--- movie-awards/src/Movies.js
+++ movie-awards/src/Movies.js
@@ -4,40 +4,18 @@ import Movie from './Movie';
 // export default function Movies(handleSearch) {
 export default class Movies extends React.Component {
 
-
-    // const movieInput = handleSearch()
-    // const url = `http://www.omdbapi.com/?s=${movieInput}&plot=full&apikey=d78f4d96`
-    // console.log(url)
-
-
-    // // console.log(movies)
-    // let movieArr = Array.from(movies);
-    // return (
-    //     movieArr.map(movie => {
-    //         return <Movie key={movie.imdbID} movie={movie} />
-    //     })
-    // )
-
     state = {
         loading: true,
         movies: null,
     };
 
-    async componentDidMount(props) {
-        // console.log(props)
-        // let userInput = props => (
-        //     console.log(props.name)
-        // );
-        // const CreateProfile = (props) => {...}
-
-        console.log(props)
-
-        const url = `http://www.omdbapi.com/?s=${props}&plot=full&apikey=d78f4d96`
+    async componentWillReceiveProps(newProps) {
+        const url = `http://www.omdbapi.com/?s=${newProps.name}&plot=full&apikey=d78f4d96`
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data)
         console.log(data.Search)
         this.setState({ movies: data.Search, loading: false });
+        console.log('got new props')
     }
 
     render() {
@@ -48,14 +26,6 @@ export default class Movies extends React.Component {
         if (!this.state.movies) {
             return <div>didn't get movies</div>;
         }
-
-        // return (
-        //     <div>
-        //         <div>{this.state.movie.Title}</div>
-        //         <div>{this.state.movie.Year}</div>
-        //         <img src={this.state.movie.Poster} />
-        //     </div>
-        // );
 
         return (
             this.state.movies.map(movie => {
