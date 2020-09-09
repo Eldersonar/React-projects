3aa5df8f692b3a6164142cb5003c3a8e6414543c
Slava Nazarenko <slavaecu@gmail.com>
2020-09-08 22:31:35 -0600

    Updates

---
+++ precache-manifest.5f9da06a00835362cf08383b0ccdc441.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6053f5629486f3df8ea9ac1f52dac906",
+    "url": "/movie-awards/index.html"
+  },
+  {
+    "revision": "de38053346e4250209db",
+    "url": "/movie-awards/static/css/main.b9531f16.chunk.css"
+  },
+  {
+    "revision": "4c448cea0524a89fcb63",
+    "url": "/movie-awards/static/js/2.60b91299.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-awards/static/js/2.60b91299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de38053346e4250209db",
+    "url": "/movie-awards/static/js/main.2e8a0ba4.chunk.js"
+  },
+  {
+    "revision": "ab4286fc218342f87273",
+    "url": "/movie-awards/static/js/runtime-main.57fafd34.js"
+  }
+]);
